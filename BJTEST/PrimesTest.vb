077af07fb1280a9a36471ff996c99c9bfcc303ac
BJTestGit <lbg@mhit.url.com.tw>
2022-04-09 17:14:06 +0800

    無瑕的程式碼 學習 - CH.05 重構 與 單元測試

--- BJTEST/PrimesTest.vb
+++ BJTEST/PrimesTest.vb
@@ -8,21 +8,21 @@ Namespace BJTEST
         <TestMethod>
         Public Sub TestGeneratePrimes()
 
-            Dim nullArray = Primes.GeneratePrimes.GeneratePrimeNumbers(0)
+            Dim nullArray = Primes.PrimeGenerator.GeneratePrimeNumbers(0)
             Assert.AreEqual(0, nullArray.Length)
 
             '第一俥質數是 2
-            Dim minArray = Primes.GeneratePrimes.GeneratePrimeNumbers(2)
+            Dim minArray = Primes.PrimeGenerator.GeneratePrimeNumbers(2)
             Assert.AreEqual(1, minArray.Length)
             Assert.AreEqual(2, minArray(0))
 
 
-            Dim threeArray = Primes.GeneratePrimes.GeneratePrimeNumbers(3)
+            Dim threeArray = Primes.PrimeGenerator.GeneratePrimeNumbers(3)
             Assert.AreEqual(2, threeArray.Length)
             Assert.AreEqual(2, threeArray(0))
             Assert.AreEqual(3, threeArray(1))
 
-            Dim centArray = Primes.GeneratePrimes.GeneratePrimeNumbers(100)
+            Dim centArray = Primes.PrimeGenerator.GeneratePrimeNumbers(100)
             Assert.AreEqual(25, centArray.Length)
             Assert.AreEqual(97, centArray(24))
 
---
+++ Primes/PrimeGenerator.vb
@@ -0,0 +1,82 @@
+﻿''' <summary>
+''' 質數產生器
+''' 1. 無瑕的程式碼 學習 - CH.05 重構 與 單元測試
+''' </summary>
+Public Class PrimeGenerator
+
+    Private Shared limitNumber As Integer
+    Private Shared arrNumberIsPrime() As Boolean
+    Private Shared primes() As Integer
+
+    ''' <summary>
+    ''' 產生質數
+    ''' </summary>
+    Public Shared Function GeneratePrimeNumbers(ByVal maxValue As Integer) As Integer()
+
+        If maxValue < 2 Then
+            Return Array.Empty(Of Integer)()
+        Else
+            InitializeSieve(maxValue)
+            Sieve()
+            LoadPrimes()
+            Return Primes
+        End If
+
+    End Function
+
+    Public Shared Sub InitializeSieve(ByVal maxValue As Integer)
+
+        limitNumber = maxValue
+        arrNumberIsPrime = New Boolean(limitNumber) {}
+        Dim i As Integer
+        For i = 0 To limitNumber
+            arrNumberIsPrime(i) = True
+        Next
+        ' 去除已知的非質數
+        arrNumberIsPrime(0) = False
+        arrNumberIsPrime(1) = False
+
+    End Sub
+
+
+    Private Shared Sub Sieve()
+
+        ' 過濾 -數值的倍數一定不是質數
+        Dim i, j As Integer
+        Dim iterationLimit As Double = Math.Sqrt(limitNumber)
+        For i = 2 To iterationLimit
+            If arrNumberIsPrime(i) Then
+                For j = i * 2 To limitNumber Step +i
+                    arrNumberIsPrime(j) = False
+                Next
+            End If
+        Next
+
+    End Sub
+
+    Private Shared Sub LoadPrimes()
+
+        Dim i, j As Integer
+        Dim count As Integer = -1
+
+        '有多少個質數
+        For i = 0 To limitNumber
+            If arrNumberIsPrime(i) Then
+                count += 1
+            End If
+        Next
+
+        primes = New Integer(count) {}
+
+        j = 0
+        For i = 0 To limitNumber
+            If arrNumberIsPrime(i) Then
+                primes(j) = i
+                j += 1
+            End If
+        Next
+
+    End Sub
+
+
+End Class
